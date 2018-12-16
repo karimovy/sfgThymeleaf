a702e5128fd33b4f9998f63e69563dd99c52f833
karimovy <abdelkrim.ali-bey.1@etsmtl.net>
2018-12-16 16:24:32 -0500

    login without spring security

--- src/main/java/guru/springframework/controller/LoginController.java
+++ src/main/java/guru/springframework/controller/LoginController.java
@@ -1,41 +1,43 @@
 package guru.springframework.controller;
 
-import javax.validation.Valid;
-
+import guru.springframework.comands.LoginCommand;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import guru.springframework.comands.CheckoutCommand;
-import guru.springframework.comands.LoginCommand;
+import javax.validation.Valid;
 
+
+/**
+ * Created by jt on 2/2/16.
+ */
 @Controller
 @RequestMapping("/thymeleaf")
 public class LoginController {
 
-	@RequestMapping("/login")
-	public String showLoginForm(Model model) {
-		
-		model.addAttribute("loginCommand", new LoginCommand());
-		
-		return "loginform";
-	}
-	
-	@RequestMapping("logout-success")
-	private String yourLoggedOut() {
-		return "logout-success";
+    @RequestMapping("/login")
+    public String showLoginForm(Model model){
 
-	}
-	
-	//@PostMapping("/dologin")
-	public String doLogin(@Valid LoginCommand loginCommand, BindingResult bindingResult) {
-		
-		if(bindingResult.hasErrors()){
-			return "loginform";
-		}
-		
-		return "redirect:/thymeleaf/";
-	}
+        model.addAttribute("loginCommand", new LoginCommand());
+
+        return "loginform";
+    }
+
+    @RequestMapping("logout-success")
+    public String yourLoggedOut(){
+
+        return "logout-success";
+    }
+
+    @RequestMapping(value = "/dologin", method = RequestMethod.POST)
+    public String doLogin(@Valid LoginCommand loginCommand, BindingResult bindingResult){
+System.out.println("test AAAAAAAAAAAAAAAAAAAAAAAA");
+        if(bindingResult.hasErrors()){
+            return "loginform";
+        }
+
+        return "redirect:/thymeleaf/";
+    }
 }
